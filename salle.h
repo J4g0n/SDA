e362b772dfad7fdff9509b8d98a2198e3bf45976
pallamidessi <sangdragon777@gmail.com>
2012-03-18 14:54:46 +0100

    Ajout #define pour intervalle des foncs aleatoires
    Ajout champs taille a la struc salle, pour les traitement aleatoire

--- salle.h
+++ salle.h
@@ -2,9 +2,17 @@
 #define __salle_h_
 #include "objet.h"
 
+#define MIN_TAILLE 5      // Taille minimal des salles
+#define MAX_TAILLE 15      //Taille maximal des salles
+#define MIN_COFFRE 0      // Nombre minimal de coffres
+#define MAX_COFFRE 2       // Nombre maximal de coffres
+#define MIN_MONSTRE 1      // Nombre minimal de monstres
+#define MAX_MONSTRE 3      // Nombre maximal de monstres
+
 typedef struct salle {
 	int numero;
 	zone **z;
+	int taille;      // vu que c'est creer aleatoirement mieux vos stocker la taille de la salle dans la str
 } *salle, strSalle;
 
 salle creerSalle();
